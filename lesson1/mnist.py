283bc18b80378548b5855388f7923def660cdc0d
Amir Hadifar <hadifar.amir@gmail.com>
2018-07-24 00:51:29 +0430

    lesson 1 finished

--- lesson1/mnist.py
+++ lesson1/mnist.py
@@ -19,17 +19,37 @@ import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# normalize data [images are gray-scale 64*64 pixels]
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
+    # convert 64*64 into 4096
     tf.keras.layers.Flatten(),
+    # MLP layer with 512 neuron
     tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    # some drop out for avoid over-fitting
     tf.keras.layers.Dropout(0.2),
+    # prediction layer (10 classes to predict)
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
+# what is the difference between categorical_crossentropy and sparse_categorical_crossentropy ?
+# categorical_crossentropy -> one-hot
+# sparse_categorical_crossentropy -> integer-hot
+
+# categorical_crossentropy
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train, tf.keras.utils.to_categorical(y_train, 10), epochs=5)
+print(model.evaluate(x_test, tf.keras.utils.to_categorical(y_test, 10)))
+
+# sparse_categorical_crossentropy
+# model.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+# model.fit(x_train, y_train, epochs=5)
+# print(model.evaluate(x_test, y_test))
+
+# evaluate method returns the loss & metrics values for the model in test mode.
