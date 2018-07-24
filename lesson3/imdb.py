0394e5338b672e93eea01d4b5895f73daea62b47
Amir Hadifar <hadifar.amir@gmail.com>
2018-07-25 00:14:03 +0430

    lesson 3 partially added

---
+++ lesson3/imdb.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 Amir Hadifar. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import tensorflow as tf
+
+imdb = tf.keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+word_index = imdb.get_word_index()
+
+word_index = {k: (v + 3) for k, v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
+print(max([len(seq) for seq in train_data]))
+print(min([len(seq) for seq in train_data]))
+
+train_data = tf.keras.preprocessing.sequence.pad_sequences(train_data, maxlen=256, padding='post',
+                                                           value=word_index["<PAD>"])
+test_data = tf.keras.preprocessing.sequence.pad_sequences(test_data, maxlen=256, padding='post',
+                                                          value=word_index["<PAD>"])
+
+val_data = train_data[:10000]
+val_label = train_labels[:10000]
+
+train_data = train_data[10000:]
+train_labels = train_labels[10000:]
+
+print(train_data[0])
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Embedding(10000, 16))
+model.add(tf.keras.layers.GlobalAveragePooling1D())
+model.add(tf.keras.layers.Dense(16, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1, activation=tf.nn.sigmoid))
+
+model.summary()
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_data,
+                    train_labels,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(val_data, val_label),
+                    verbose=1)
+
+eval = model.evaluate(test_data, test_labels, batch_size=5)
+print(eval)
+history_dict = history.history
+history_dict.keys()
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
