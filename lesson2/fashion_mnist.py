262930c3703e6f7cbdfc6c7db31a3633337d3095
Amir Hadifar <hadifar.amir@gmail.com>
2018-07-24 22:13:01 +0430

    fashion_mnist added

---
+++ lesson2/fashion_mnist.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 Amir Hadifar. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+mnist = tf.keras.datasets.fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, tf.keras.utils.to_categorical(y_train, 10), epochs=5)
+print(model.evaluate(x_test, tf.keras.utils.to_categorical(y_test, 10)))
+
+predictions = model.predict(x_test)
+print(predictions[0].argmax(), y_test[0])
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off')
+    plt.imshow(x_test[i], cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions[i])
+    true_label = y_test[i]
+    if predicted_label == true_label:
+        color = 'green'
+    else:
+        color = 'red'
+    plt.xlabel("{} ({})".format(class_names[predicted_label],
+                                class_names[true_label]),
+               color=color)
+plt.show()
