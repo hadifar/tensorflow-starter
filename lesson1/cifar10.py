e85474429444ec7617579756cd7e9291d6893c83
Amir Hadifar <hadifar.amir@gmail.com>
2018-07-30 21:37:20 +0430

    cnn visualization added

--- lesson1/cifar10.py
+++ lesson1/cifar10.py
@@ -14,7 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 from tensorflow import keras
+from tensorflow.python.keras import Model
+
+matplotlib.pyplot.ioff()
 
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -42,5 +49,49 @@ if __name__ == '__main__':
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=20)
+    model.fit(x_train, y_train, epochs=1)
     print(model.evaluate(x_test, y_test))
+
+    successive_outputs = [layer.output for layer in model.layers[1:]]
+    visualization_model = Model(img_input, successive_outputs)
+
+    x = x_test[1]
+    x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+
+    # Rescale by 1/255
+    # x /= 255
+
+    # Let's run our image through our network, thus obtaining all
+    # intermediate representations for this image.
+    successive_feature_maps = visualization_model.predict(x)
+
+    # These are the names of the layers, so can have them as part of our plot
+    layer_names = [layer.name for layer in model.layers[1:]]
+
+    # Now let's display our representations
+    for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+        if len(feature_map.shape) == 4:
+            # Just do this for the conv / maxpool layers, not the fully-connected layers
+            n_features = feature_map.shape[-1]  # number of features in feature map
+            # The feature map has shape (1, size, size, n_features)
+            size = feature_map.shape[1]
+            # We will tile our images in this matrix
+            display_grid = np.zeros((size, size * n_features))
+            for i in range(n_features):
+                # Postprocess the feature to make it visually palatable
+                x = feature_map[0, :, :, i]
+                x -= x.mean()
+                x /= x.std()
+                x *= 64
+                x += 128
+                x = np.clip(x, 0, 255).astype('uint8')
+                # We'll tile each filter into this big horizontal grid
+                display_grid[:, i * size: (i + 1) * size] = x
+            # Display the grid
+            scale = 20. / n_features
+            plt.figure(figsize=(scale * n_features, scale))
+            plt.title(layer_name)
+            plt.grid(False)
+            plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+    # plt.show()
