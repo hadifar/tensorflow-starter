57ffafe201da171f1a05262e500f8208a0a99745
Amir Hadifar <hadifar.amir@gmail.com>
2018-08-16 10:26:06 +0430

    cnn simplified a bit

--- lesson3/imdb_cnn_tf.py
+++ lesson3/imdb_cnn_tf.py
@@ -30,19 +30,8 @@ dropout = 0.25
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=vocabulary_size)
 
-word_index = imdb.get_word_index()
-
-word_index = {k: (v + 3) for k, v in word_index.items()}
-word_index["<PAD>"] = 0
-word_index["<START>"] = 1
-word_index["<UNK>"] = 2  # unknown
-word_index["<UNUSED>"] = 3
-reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-
-train_data = tf.keras.preprocessing.sequence.pad_sequences(train_data, maxlen=seq_len, padding='post',
-                                                           value=word_index["<PAD>"])
-test_data = tf.keras.preprocessing.sequence.pad_sequences(test_data, maxlen=seq_len, padding='post',
-                                                          value=word_index["<PAD>"])
+train_data = tf.keras.preprocessing.sequence.pad_sequences(train_data, maxlen=seq_len)
+test_data = tf.keras.preprocessing.sequence.pad_sequences(test_data, maxlen=seq_len)
 
 train_labels = tf.keras.utils.to_categorical(train_labels)
 test_labels = tf.keras.utils.to_categorical(test_labels)
