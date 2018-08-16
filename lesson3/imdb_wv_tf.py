849e91e339830299f58e8c93aa57d2a54b11ae02
Amir Hadifar <hadifar.amir@gmail.com>
2018-08-16 10:38:49 +0430

    imdb word vector simplified

--- lesson3/imdb_wv_tf.py
+++ lesson3/imdb_wv_tf.py
@@ -25,13 +25,9 @@ vocab_size = 10000
 embed_size = 32
 seq_len = 256
 batch_size = 512
-training_iters = 1000000
-epoch = 40
+nb_epoch = 500
 
 (train_x, train_y), (test_x, test_y) = imdb.load_data(num_words=vocab_size)
-
-# train_y = tf.keras.utils.to_categorical(train_y)
-# test_y = tf.keras.utils.to_categorical(test_y)
 
 train_x = tf.keras.preprocessing.sequence.pad_sequences(train_x, maxlen=seq_len)
 test_x = tf.keras.preprocessing.sequence.pad_sequences(test_x, maxlen=seq_len)
@@ -42,48 +38,55 @@ data_helper = DataHelper(train_x, train_y)
 X = tf.placeholder(dtype=tf.int32, shape=[None, seq_len])
 Y = tf.placeholder(dtype=tf.int32, shape=[None, 1])
 
+global_step = tf.Variable(0, trainable=False)
+
 # Model
 embedding = tf.Variable(tf.truncated_normal([vocab_size, embed_size]))
 inputs = tf.nn.embedding_lookup(embedding, X)  # [batch_size * 256 * 100]
 
+# Pooling
 pooling = tf.layers.max_pooling1d(inputs, 2, strides=1, padding="valid")  # [?,32]
 global_pooling = tf.reduce_mean(pooling, 1)
+
+# Dense layer
 dense1 = tf.layers.dense(global_pooling, 16, activation=tf.nn.relu)
+
+# Prediction
 logit = tf.layers.dense(dense1, 1)
 pred = tf.round(tf.nn.sigmoid(logit))
-# pred = tf.cast(pred, tf.int32)
+
+# Calculate Accuracy
 correct_pred = tf.equal(pred, tf.cast(Y, tf.float32))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
+# Cost and optimizer
 cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.cast(Y, tf.float32), logits=logit))
-optimizer = tf.train.AdamOptimizer().minimize(cost)
+optimizer = tf.train.AdamOptimizer()
+train_step = optimizer.minimize(cost, global_step=global_step)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    step = 1
-    while step * batch_size < training_iters:
 
-        # We will read a batch of 100 images [100 x 784] as batch_x
-        # batch_y is a matrix of [100x10]
+    while True:
+
         batch_x, batch_y = data_helper.next_batch(batch_size)
         batch_y = np.expand_dims(batch_y, 1)
 
-        # We consider each row of the image as one sequence
-        # Reshape data to get 28 seq of 28 elements, so that, batxh_x is [100x28x28]
-        # batch_x = batch_x.reshape((batch_size, n_steps, n_input))
+        _, step = sess.run([train_step, global_step], feed_dict={X: batch_x, Y: batch_y})
+        current_step = tf.train.global_step(sess, global_step)
 
-        # Run optimization op (backprop)
-        sess.run(optimizer, feed_dict={X: batch_x, Y: batch_y})
-
-        if step % batch_size == 0:
+        if step % 10 == 0:
             # Calculate batch accuracy
             acc = sess.run(accuracy, feed_dict={X: batch_x, Y: batch_y})
             # Calculate batch loss
             loss = sess.run(cost, feed_dict={X: batch_x, Y: batch_y})
-            print("Iter " + str(step * batch_size) + ", Minibatch Loss= " +
+            print("Iter " + str(current_step) + ", Minibatch Loss= " +
                   "{:.6f}".format(loss) + ", Training Accuracy= " +
                   "{:.5f}".format(acc))
-        step += 1
+
+        if nb_epoch < current_step:
+            break
+
     print("Optimization Finished!")
     test_helper = DataHelper(test_x, test_y)
     step = 0
