6c6cf3ee5671c2ea22e6d273c0e0a4382dd7dd84
Amir Hadifar <hadifar.amir@gmail.com>
2018-08-17 21:19:01 +0430

    question 2 is done

--- lesson6/assignment1/q2.py
+++ lesson6/assignment1/q2.py
@@ -14,3 +14,124 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+import time
+# tf.enable_eager_execution()
+
+from lesson6 import utils
+
+# Define paramaters for the model
+learning_rate = 0.01
+batch_size = 128
+n_epochs = 30
+n_train = 60000
+n_test = 10000
+
+# Step 1: Read in data
+mnist_folder = '/Users/mac/PycharmProjects/tensorflow-starter/lesson6/assignment1/data'
+utils.download_mnist(mnist_folder)
+train, val, test = utils.read_mnist(mnist_folder, flatten=True)
+
+# Step 2: Create datasets and iterator
+# create training Dataset and batch it
+train_data = tf.data.Dataset.from_tensor_slices(train)
+train_data = train_data.shuffle(10000)  # if you want to shuffle your data
+train_data = train_data.batch(batch_size)
+
+# create testing Dataset and batch it
+#############################
+########## TO DO ############
+#############################
+test_data = tf.data.Dataset.from_tensor_slices(test)
+test_data = test_data.shuffle(10000)
+test_data = test_data.batch(batch_size)
+
+# create one iterator and initialize it with different datasets
+iterator = tf.data.Iterator.from_structure(train_data.output_types,
+                                           train_data.output_shapes)
+img, label = iterator.get_next()
+
+train_init = iterator.make_initializer(train_data)  # initializer for train_data
+test_init = iterator.make_initializer(test_data)  # initializer for train_data
+
+# Step 3: create weights and bias
+# w is initialized to random variables with mean of 0, stddev of 0.01
+# b is initialized to 0
+# shape of w depends on the dimension of X and Y so that Y = tf.matmul(X, w)
+# shape of b depends on Y
+#############################
+########## TO DO ############
+#############################
+
+w = tf.get_variable(name='weight',
+                    initializer=tf.truncated_normal(shape=[784, 10], mean=0, stddev=0.01))
+b = tf.get_variable(name='bias', initializer=tf.zeros([10]))
+
+# Step 4: build model
+# the model that returns the logits.
+# this logits will be later passed through softmax layer
+logits = tf.matmul(img, w) + b
+#############################
+########## TO DO ############
+#############################
+
+
+# Step 5: define loss function
+# use cross entropy of softmax of logits as the loss function
+loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=label, logits=logits)
+loss = tf.reduce_mean(loss)
+#############################
+########## TO DO ############
+#############################
+
+
+# Step 6: define optimizer
+# using Adamn Optimizer with pre-defined learning rate to minimize loss
+optimizer = tf.train.AdamOptimizer().minimize(loss)
+#############################
+########## TO DO ############
+#############################
+
+
+# Step 7: calculate accuracy with test set
+preds = tf.nn.softmax(logits)
+correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(label, 1))
+accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
+
+writer = tf.summary.FileWriter('./graphs/logreg', tf.get_default_graph())
+with tf.Session() as sess:
+    start_time = time.time()
+    sess.run(tf.global_variables_initializer())
+
+    # train the model n_epochs times
+    for i in range(n_epochs):
+        sss = sess.run(train_init)  # drawing samples from train_data
+        total_loss = 0
+        n_batches = 0
+        try:
+            while True:
+                _, l = sess.run([optimizer, loss])
+                total_loss += l
+                n_batches += 1
+        except tf.errors.OutOfRangeError:
+            pass
+        print('Average loss epoch {0}: {1}'.format(i, total_loss / n_batches))
+    print('Total time: {0} seconds'.format(time.time() - start_time))
+
+    # test the model
+    sess.run(test_init)  # drawing samples from test_data
+    total_correct_preds = 0
+    try:
+        while True:
+            accuracy_batch = sess.run(accuracy)
+            total_correct_preds += accuracy_batch
+    except tf.errors.OutOfRangeError:
+        pass
+
+    print('Accuracy {0}'.format(total_correct_preds / n_test))
+writer.close()
