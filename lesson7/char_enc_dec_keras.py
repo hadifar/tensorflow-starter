5fe013b2d7062630973d25759e4a9e788b695d9d
Amir Hadifar <hadifar.amir@gmail.com>
2018-09-18 00:58:37 +0430

    keras implementation of enc_dec completed

--- lesson7/char_enc_dec_keras.py
+++ lesson7/char_enc_dec_keras.py
@@ -1,45 +1,3 @@
-'''Sequence to sequence example in Keras (character-level).
-This script demonstrates how to implement a basic character-level
-sequence-to-sequence model. We apply it to translating
-short English sentences into short French sentences,
-character-by-character. Note that it is fairly unusual to
-do character-level machine translation, as word-level
-models are more common in this domain.
-# Summary of the algorithm
-- We start with input sequences from a domain (e.g. English sentences)
-    and corresponding target sequences from another domain
-    (e.g. French sentences).
-- An encoder LSTM turns input sequences to 2 state vectors
-    (we keep the last LSTM state and discard the outputs).
-- A decoder LSTM is trained to turn the target sequences into
-    the same sequence but offset by one timestep in the future,
-    a training process called "teacher forcing" in this context.
-    Is uses as initial state the state vectors from the encoder.
-    Effectively, the decoder learns to generate `targets[t+1...]`
-    given `targets[...t]`, conditioned on the input sequence.
-- In inference mode, when we want to decode unknown input sequences, we:
-    - Encode the input sequence into state vectors
-    - Start with a target sequence of size 1
-        (just the start-of-sequence character)
-    - Feed the state vectors and 1-char target sequence
-        to the decoder to produce predictions for the next character
-    - Sample the next character using these predictions
-        (we simply use argmax).
-    - Append the sampled character to the target sequence
-    - Repeat until we generate the end-of-sequence character or we
-        hit the character limit.
-# Data download
-English to French sentence pairs.
-http://www.manythings.org/anki/fra-eng.zip
-Lots of neat sentence pairs datasets can be found at:
-http://www.manythings.org/anki/
-# References
-- Sequence to Sequence Learning with Neural Networks
-    https://arxiv.org/abs/1409.3215
-- Learning Phrase Representations using
-    RNN Encoder-Decoder for Statistical Machine Translation
-    https://arxiv.org/abs/1406.1078
-'''
 from __future__ import print_function
 
 import os
@@ -205,8 +163,7 @@ def decode_sequence(input_seq):
     stop_condition = False
     decoded_sentence = ''
     while not stop_condition:
-        output_tokens, h, c = decoder_model.predict(
-            [target_seq] + states_value)
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
 
         # Sample a token
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
--- lesson7/word_enc_dec_keras.py
+++ lesson7/word_enc_dec_keras.py
@@ -22,14 +22,17 @@ import os
 import nltk
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Input, GRU, Lambda, Dense, CuDNNGRU
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
 
 tf.reset_default_graph()
 
-################################################################################
-################################################################################
-################################################################################
-# nb_examples = 118964
-nb_examples = 100
+###############################################################################
+###############################################################################
+###############################################################################
+nb_examples = 118964
 
 path_to_zip = tf.keras.utils.get_file(
     'spa-eng.zip', origin='http://download.tensorflow.org/data/spa-eng.zip',
@@ -70,8 +73,8 @@ target_sequences = spa_tokenizer.texts_to_sequences(target_texts)
 max_inp_seq = max([len(txt) for txt in input_sequences])
 max_trg_seq = max([len(txt) for txt in target_sequences])
 
-eng_vocab_size = len(eng_tokenizer.word_index)
-spa_vocab_size = len(spa_tokenizer.word_index)
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+spa_vocab_size = len(spa_tokenizer.word_index) + 1
 
 print('max sequence length in language 1', max_inp_seq)
 print('max sequence length in language 2', max_trg_seq)
@@ -80,10 +83,74 @@ input_sequences = tf.keras.preprocessing.sequence.pad_sequences(input_sequences,
 target_sequences = tf.keras.preprocessing.sequence.pad_sequences(target_sequences, max_trg_seq)
 output_sequences = np.zeros_like(target_sequences)
 output_sequences[:, :max_trg_seq - 1] = target_sequences[:, 1:]
+output_sequences = tf.keras.utils.to_categorical(output_sequences, spa_vocab_size)
+
+spa_index_to_word = dict([(value, key) for (key, value) in spa_tokenizer.word_index.items()])
+eng_index_to_word = dict([(value, key) for (key, value) in eng_tokenizer.word_index.items()])
+
 
 ################################################################################
 ################################################################################
 ################################################################################
 
 
+def one_hot(x, num_classes):
+    return K.one_hot(x, num_classes=num_classes)
 
+
+enc_size = 128
+dec_size = 128
+
+# encoder part
+enc_inp = Input(shape=(max_inp_seq,), dtype=tf.int32)
+enc_one_hot = Lambda(one_hot, arguments={'num_classes': eng_vocab_size})(enc_inp)
+if tf.test.is_gpu_available():
+    enc_output, enc_state = CuDNNGRU(units=enc_size, return_state=True)(enc_one_hot)
+else:
+    enc_output, enc_state = GRU(units=enc_size, return_state=True)(enc_one_hot)
+
+# decoder part
+dec_inp = Input(shape=(max_trg_seq,), dtype=tf.int32)
+dec_one_hot = Lambda(one_hot, arguments={'num_classes': spa_vocab_size})(dec_inp)
+if tf.test.is_gpu_available():
+    dec_output = CuDNNGRU(units=dec_size, return_sequences=True)(dec_one_hot, initial_state=enc_state)
+else:
+    dec_output = GRU(units=dec_size, return_sequences=True)(dec_one_hot, initial_state=enc_state)
+pred = Dense(spa_vocab_size, activation='softmax')(dec_output)
+
+# compile and fit
+model = Model(inputs=[enc_inp, dec_inp], outputs=pred)
+model.compile(optimizer=Adam(0.005), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit([input_sequences, target_sequences], output_sequences, batch_size=128, epochs=100)
+
+# save model
+model.save('s2s.hd5')
+
+################################################################################
+################################################################################
+################################################################################
+# retrieve model
+new_model = tf.keras.models.load_model('s2s.hd5')
+
+
+def translate(inp_sequence):
+    generated = ''
+    new_state = np.zeros(max_trg_seq)
+    i = 1
+    while True:
+        p = new_model.predict([np.expand_dims(inp_sequence, 0), np.expand_dims(new_state, 0)])
+        p_id = p[0][i].argmax()
+        if p_id != 0:
+            generated = generated + ' ' + spa_index_to_word[p_id]
+            new_state[i - 1] = p_id
+            i = i + 1
+        else:
+            break
+    return generated
+
+
+for sequence in range(32):
+    print(" ".join([eng_index_to_word[t] for t in input_sequences[sequence] if t != 0]))
+    print(" ".join([spa_index_to_word[t] for t in target_sequences[sequence] if t != 0]))
+    print(translate(input_sequences[sequence]))
+    print(50 * '-')
