d0de20a209c939a9068f2952d3b7eada857cb921
Amir Hadifar <hadifar.amir@gmail.com>
2018-09-20 11:40:34 +0430

    word level s2s completed

--- lesson7/word_enc_dec_keras.py
+++ lesson7/word_enc_dec_keras.py
@@ -23,7 +23,9 @@ import nltk
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Input, GRU, Lambda, Dense, CuDNNGRU
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import GRU, CuDNNGRU
+from tensorflow.keras.layers import Input, Lambda
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.optimizers import Adam
 
@@ -33,7 +35,8 @@ tf.reset_default_graph()
 ###############################################################################
 ###############################################################################
 
-nb_examples = 10000
+nb_examples = 1024
+nb_epochs = 100
 
 path_to_zip = tf.keras.utils.get_file(
     'spa-eng.zip', origin='http://download.tensorflow.org/data/spa-eng.zip',
@@ -110,31 +113,40 @@ enc_size = 256
 dec_size = 256
 
 # encoder part
-enc_inp = Input(shape=(max_inp_seq,), dtype=tf.int32)
+enc_inp = Input(shape=(None,), dtype=tf.int32)
 # our inputs are sparse but we need one-hot encoding
-enc_one_hot = Lambda(one_hot, arguments={'num_classes': eng_vocab_size})(enc_inp)
+enc_one_hot = Lambda(function=one_hot,
+                     arguments={'num_classes': eng_vocab_size},
+                     output_shape=(max_inp_seq, eng_vocab_size))(enc_inp)
 # use CuDNNGRU if available, is 3x faster
 if tf.test.is_gpu_available():
-    enc_output, enc_state = CuDNNGRU(units=enc_size, return_state=True)(enc_one_hot)
+    enc_gru = CuDNNGRU(units=enc_size, return_state=True)
+    enc_output, enc_state = enc_gru(enc_one_hot)
 else:
-    enc_output, enc_state = GRU(units=enc_size, return_state=True)(enc_one_hot)
+    enc_gru = GRU(units=enc_size, return_state=True)
+    enc_output, enc_state = enc_gru(enc_one_hot)
 
 # decoder part
-dec_inp = Input(shape=(max_trg_seq,), dtype=tf.int32)
+dec_inp = Input(shape=(None,), dtype=tf.int32)
 # our outputs are sparse but we need one-hot encoding
-dec_one_hot = Lambda(one_hot, arguments={'num_classes': spa_vocab_size})(dec_inp)
+dec_one_hot = Lambda(function=one_hot,
+                     arguments={'num_classes': spa_vocab_size},
+                     output_shape=(max_trg_seq, spa_vocab_size))(dec_inp)
 # use CuDNNGRU if available, is 3x faster
 if tf.test.is_gpu_available():
-    dec_output = CuDNNGRU(units=dec_size, return_sequences=True)(dec_one_hot, initial_state=enc_state)
+    dec_gru = CuDNNGRU(units=dec_size, return_sequences=True, return_state=True)
+    dec_output, _ = dec_gru(dec_one_hot, initial_state=enc_state)
 else:
-    dec_output = GRU(units=dec_size, return_sequences=True)(dec_one_hot, initial_state=enc_state)
+    dec_gru = GRU(units=dec_size, return_sequences=True, return_state=True)
+    dec_output, _ = dec_gru(dec_one_hot, initial_state=enc_state)
 
-pred = Dense(spa_vocab_size, activation='softmax')(dec_output)
+dec_dense = Dense(spa_vocab_size, activation='softmax')
+pred = dec_dense(dec_output)
 
 # compile and fit
 model = Model(inputs=[enc_inp, dec_inp], outputs=pred)
 model.compile(optimizer=Adam(0.005), loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit([enc_sequence_inps, dec_sequence_inps], dec_sequence_outputs, batch_size=128, epochs=100)
+model.fit([enc_sequence_inps, dec_sequence_inps], dec_sequence_outputs, batch_size=128, epochs=nb_epochs)
 
 # save model
 model.save('s2s.hd5')
@@ -143,30 +155,66 @@ model.save('s2s.hd5')
 ################################################################################
 ################################################################################
 # retrieve model
-new_model = load_model('s2s.hd5')
+model = load_model('s2s.hd5')
+
+encoder_inputs = model.input[0]  # input_1
+_, encoder_states = model.layers[4].output  # gru_1
+encoder_model = Model(encoder_inputs, encoder_states)
+
+decoder_inputs = model.input[1]  # input_2
+decoder_one_hot = model.layers[3](decoder_inputs)
+decoder_states_inputs = [Input(shape=(256,), name='input_3')]
+decoder_gru = model.layers[5]
+decoder_outputs, decoder_states = decoder_gru(decoder_one_hot, initial_state=decoder_states_inputs)
+decoder_dense = model.layers[6]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + [decoder_states])
 
 
-# inference
-def translate(inp_sequence):
-    final_translation = ''
-    # fill state with zero for beginning
-    new_state = np.zeros(max_trg_seq)
-    i = 0
-    while True:
-        p = new_model.predict([np.expand_dims(inp_sequence, 0), np.expand_dims(new_state, 0)])
-        p_id = p[0][i].argmax()
-        if p_id != 0:
-            final_translation = final_translation + ' ' + spa_index_to_word[p_id]
-            new_state[i] = p_id
-            i = i + 1
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0][0] = 0
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, new_state = decoder_model.predict([target_seq] + [states_value])
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        if sampled_token_index == 0:
+            sample_word = ''
         else:
-            break
-    return final_translation
+            sample_word = spa_index_to_word[sampled_token_index]
+        decoded_sentence += sample_word + ' '
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if sample_word == '</s>' or len(decoded_sentence.split()) > max_trg_seq:
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1))
+        target_seq[0][0] = sampled_token_index
+
+        # Update states
+        states_value = new_state
+
+    return decoded_sentence
 
 
-# pick 32 sentences from corpus then translate them into spanish
-for seq_i in range(32):
-    print(" ".join([eng_index_to_word[t] for t in enc_sequence_inps[seq_i] if t != 0]))
-    print(" ".join([spa_index_to_word[t] for t in dec_sequence_inps[seq_i] if t != 0]))
-    print(translate(enc_sequence_inps[seq_i]))
-    print(50 * '-')
+for seq_index in range(32):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = enc_sequence_inps[seq_index: seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print('-')
+    print('Input sentence:', input_texts[seq_index])
+    print('Decoded sentence:', decoded_sentence)
