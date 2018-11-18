f71e61e917a3043447e29fe0150a2a75e71df253
Amir Hadifar <hadifar.amir@gmail.com>
2018-11-19 00:22:43 +0330

    inital commit on drl

--- lesson15/simple.py
+++ lesson15/simple.py
@@ -14,3 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+import gym
+
+env = gym.make('MountainCar-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample())
